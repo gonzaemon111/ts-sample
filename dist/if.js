6ce8ae3f4d488ca9fbc43a041a68ed982c566e3e
gomzaemon111 <goki14914@icloud.com>
2019-05-15 00:52:50 +0900

    add if.ts

---
+++ dist/if.js
@@ -0,0 +1,10 @@
+var value = 1;
+if (value) {
+    console.log("定義済み");
+}
+else {
+    console.log("未定義");
+}
+
+// value = 0の場合は、else
+// value = 1の場合は、if
---
+++ src/if.ts
@@ -0,0 +1,10 @@
+let value : number = 0;
+
+if (value) {
+  console.log("定義済み");
+} else {
+  console.log("未定義");
+}
+
+// value = 0の場合は、else
+// value = 1の場合は、if
