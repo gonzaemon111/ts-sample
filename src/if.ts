4c1b70cdf769dc5fc056cc68e9b567dfdf834393
gomzaemon111 <goki14914@icloud.com>
2019-05-19 12:20:02 +0900

    add --fix lint for src/if.ts

--- src/if.ts
+++ src/if.ts
@@ -1,9 +1,9 @@
-let value : number = 0;
+let value: number = 0
 
 if (value) {
-  console.log("定義済み");
+  console.log('定義済み')
 } else {
-  console.log("未定義");
+  console.log('未定義')
 }
 
 // value = 0の場合は、else
