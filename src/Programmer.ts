8df47d98b928b69d0c2d28a48653f32de35131f4
gomzaemon111 <goki14914@icloud.com>
2019-06-24 00:31:28 +0900

    fix src/Programmer.ts

--- src/Programmer.ts
+++ src/Programmer.ts
@@ -1,24 +1,21 @@
-import NewPerson from "./NewPerson";
+import NewPerson from './NewPerson'
 export default class Programmer extends NewPerson {
-  salary: number;
-  skills: Array<{ lang: string, rate: number }>;
+  salary: number
+  skills: Array<{ lang: string; rate: number }>
 
   constructor(init?: Partial<Programmer>) {
-    super();
-    Object.assign(this, init);
+    super()
+    Object.assign(this, init)
   }
 }
 
 const obje = {
-  name: "hoge",
+  name: 'hoge',
   age: 90,
   salary: 200,
-  skills: [
-    { lang: "javascript", rate: 5 },
-    { lang: "ruby", rate: 1 }
-  ]
-};
+  skills: [{ lang: 'javascript', rate: 5 }, { lang: 'ruby', rate: 1 }]
+}
 
-const hoge = new Programmer(obje);
+const hoge = new Programmer(obje)
 
 console.log(hoge.skills[0].lang) // javascript
